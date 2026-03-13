e96e8ff4bb2290c63748900694f325bb008aa332
Anil Kulkarni <anil@terminal.space>
2026-03-12 22:53:43 -0700

    Add scaffolding for the nota-bene project

---
+++ nota-bene/src/nota_bene/__init__.py
@@ -0,0 +1,3 @@
+from nota_bene.__about__ import __version__
+
+__all__ = ["__version__"]
---
+++ nota-bene/tests/test_version.py
@@ -0,0 +1,6 @@
+from nota_bene import __version__
+
+
+def test_version() -> None:
+    assert isinstance(__version__, str)
+    assert len(__version__) > 0
